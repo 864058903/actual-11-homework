d5ba9959220372d0dda443e2c87372e7786fc95c
lixhui <13264346828@163.com>
2016-09-03 01:37:11 +0800

    first commit

--- 01/lixhui/chengfa.py
+++ 01/lixhui/chengfa.py
@@ -1,7 +1,3 @@
 #!/usr/bin/evn python
 for i in range(1, 10):  
-<<<<<<< HEAD
     print " ".join(["%d*%d=%d" % (j, i, i*j) for j in range(1, i+1)]) 
-=======
-    print " ".join(["%d*%d=%d" % (j, i, i*j) for j in range(1, i+1)]) 
->>>>>>> b66a07aa9865980106cea31401f4297be29a93d5
