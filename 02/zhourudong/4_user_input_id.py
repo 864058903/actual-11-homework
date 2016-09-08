aa285b987fde42f9b09bcf8f20b72acd696d1a7b
cucyzrd <292016178@qq.com>
2016-09-08 21:55:53 +0800

    用户输入_输出格式化，添加方法2

--- 02/zhourudong/4_user_input_id.py
+++ 02/zhourudong/4_user_input_id.py
@@ -22,3 +22,19 @@ for x in a_list_of_tuple:
 	out_list.append(temp_tuple)
 
 print out_list
+
+
+# 方法2
+user_input = raw_input('please input username userid(examp tom:11,python:1112):')
+# user_input = 'nginx:2008,python:1991'
+
+while (':' not in user_input) and (',' not in user_input):
+    ''' 检查用户输入'''
+    user_input = raw_input('please input username userid(examp tom:11,python:1112):')
+
+a = user_input.split(',')
+b = [tuple(x.split(':')) for x in a]
+
+print b
+
+# [('nginx', '2008'), ('python', '1991')]
