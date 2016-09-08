5f488329895d160fc930698c00ff6770c9e065e1
seoguess <seoguess@qq.com>
2016-09-08 14:10:50 +0800

    add list reverse note

---
+++ 02/shawn/list_reverse.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#encoding: utf-8
+
+nums = [23,2,1,23,2223,232,1,133,44]
+print nums
+
+for i in range(len(nums) / 2):
+    nums[i], nums[-1 - i] = nums[-1 - i], nums[i]
+
+    
+print nums
+
+nums = [23,2,1,23,2223,232,1,133,44]
+nums.reverse()
+print nums
