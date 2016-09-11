76df1fa6ade3e073bbd436b2ba136ec013e4a902
weibzd <157856789@qq.com>
2016-09-11 11:31:05 +0800

    Update homework03.py

--- 02/wanghaibing/homework03.py
+++ 02/wanghaibing/homework03.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 alist=[1,2,3,4,2,12,3,14,3,2,12,3,14,3,21,2,2,3,4111,22,3333,4]
+print alist
 blist=[]
 for i in alist:
-    num=0
-    for j in blist:
-       if i == j:
-           num=1
-           break
-    if num == 0:
-       blist.append(i)
+       if i not in blist:
+         blist.append(i)
 print blist
+
 
 '''
 功能ok
 改进:
 1. line 5-line10是否能修改为1行
+'''
+'''
+已改
 '''
