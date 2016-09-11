92c4ea034238be371eaf0facd2d03b21d1974780
uroot666 <liangbiao666@gmail.com>
2016-09-11 11:24:28 +0800

    liangbiao xiugai 02

--- 02/liangbiao/maopao.py
+++ 02/liangbiao/maopao.py
@@ -4,11 +4,12 @@
 
 _list1 = [12,4,3,5,6,1,2,3,7,1,1,1]
 
-for i in range(len(_list1) - 1):
-	if _list1[i] > _list1[i + 1]:
-		temp = _list1[i + 1]
-		_list1[i + 1] = _list1[i]
-		_list1[i] = temp
+for j in range(len(_list1) - 1):
+	for i in (range(len(_list1) - 1 - j)):
+		if _list1[i] > _list1[i + 1]:
+			temp = _list1[i + 1]
+			_list1[i + 1] = _list1[i]
+			_list1[i] = temp
 print _list1
 
 '''
