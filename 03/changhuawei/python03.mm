9b411d20b210ea0188cf939260cddd5dab2d9e86
huaweichang <513314416@qq.com>
2016-09-13 10:43:14 +0800

    03work

---
+++ 03/changhuawei/python03.mm
@@ -0,0 +1,312 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1473680460849" ID="ID_1669740544" MODIFIED="1473680476559" TEXT="python03">
+<node CREATED="1473680491702" ID="ID_952714242" MODIFIED="1473680499100" POSITION="right" TEXT="dict">
+<node CREATED="1473680683121" ID="ID_1751878892" MODIFIED="1473680688570" TEXT="&#x5b9a;&#x4e49;">
+<node CREATED="1473680692255" ID="ID_1277550469" MODIFIED="1473680736085" TEXT="&#x7528; {} &#x62ec;&#x8d77;&#x6765;  &#xff0c;&#x6bcf;&#x4e2a;&#x5143;&#x7d20;&#x7528;&#x9017;&#x53f7;&#x5206;&#x9694;&#xff0c;key&#xff0c;value &#x5f62;&#x5f0f;"/>
+</node>
+<node CREATED="1473680752089" ID="ID_618760787" MODIFIED="1473682052129" TEXT="&#x7279;&#x6027;">
+<node CREATED="1473680825720" ID="ID_1967045240" MODIFIED="1473682124725">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      key&#24517;&#39035;&#26159;&#21807;&#19968;&#30340;&#65292;value&#21487;&#20197;&#21462;&#20219;&#20309;&#25968;&#25454;&#31867;&#22411;&#65292;&#20294;&#38190;&#24517;&#39035;&#26159;&#19981;&#21487;&#21464;&#30340;&#65292;&#22914;&#23383;&#31526;&#20018;&#65292;&#25968;&#23383;&#25110;&#20803;&#32452;
+    </p>
+    <p>
+      <font color="rgb(51, 51, 51)" face="Microsoft Yahei, Helvetica Neue, Helvetica, Arial, sans-serif">&#21019;&#24314;&#26102;&#22914;&#26524;&#21516;&#19968;&#20010;&#38190;&#34987;&#36171;&#20540;&#20004;&#27425;&#65292;&#21518;&#19968;&#20010;&#20540;&#20250;&#34987;&#35760;&#20303;</font>
+    </p>
+    <p>
+      &#20351;&#29992;key&#35775;&#38382;&#20803;&#32032;
+    </p>
+    <p>
+      &#20351;&#29992;key&#26041;&#24335;&#20462;&#25913;/&#36861;&#21152;
+    </p>
+    <p>
+      &#20351;&#29992;key&#21024;&#38500;
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1473681106335" ID="ID_1405065766" MODIFIED="1473681113534" TEXT="&#x8bbf;&#x95ee;">
+<node CREATED="1473681903572" ID="ID_1060982741" MODIFIED="1473681913943" TEXT="dit[1]"/>
+</node>
+<node CREATED="1473681114230" ID="ID_1772052694" MODIFIED="1473681354942" TEXT="&#x4fee;&#x6539;">
+<node CREATED="1473681915824" ID="ID_884589015" MODIFIED="1473681931351" TEXT="dit[1] =  2"/>
+</node>
+<node CREATED="1473681360025" ID="ID_249868986" MODIFIED="1473681364927" TEXT="&#x5220;&#x9664;">
+<node CREATED="1473681935683" ID="ID_1956600010" MODIFIED="1473681950109" TEXT="del dit[1]"/>
+</node>
+<node CREATED="1473681393167" ID="ID_1866453984" MODIFIED="1473681395141" TEXT="in">
+<node CREATED="1473681953122" ID="ID_1959545668" MODIFIED="1473681972790" TEXT="&apos;2&apos; in dit">
+<node CREATED="1473682192869" ID="ID_1111011599" MODIFIED="1473682209179" TEXT="&#x5224;&#x65ad;&#x662f;&#x5426;&#x5728;"/>
+</node>
+</node>
+<node CREATED="1473682866485" ID="ID_1341049374" MODIFIED="1473682873784" TEXT="for in ">
+<node CREATED="1473682875372" ID="ID_585003622" MODIFIED="1473682884118" TEXT="&#x53ef;&#x904d;&#x5386;&#x7684;"/>
+</node>
+<node CREATED="1473681849598" ID="ID_199104746" MODIFIED="1473681856873" TEXT="&#x57fa;&#x672c;&#x6570;&#x636e;&#x7c7b;&#x578b;">
+<node CREATED="1473681857959" ID="ID_1111698434" MODIFIED="1473681880517">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      int,float,str,None,bool(True,Flase,)
+    </p>
+    <p>
+      tuple &#20803;&#32452;&#65292;&#19981;&#21487;&#21464;&#30340;
+    </p>
+    <p>
+      key&#65306;&#19981;&#33021;&#20462;&#25913;&#30340;&#25968;&#25454;&#31867;&#22411;&#21487;&#20197;&#20316;key
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1473682229794" ID="ID_756591264" MODIFIED="1473682548071" TEXT="&#x51fd;&#x6570;">
+<node CREATED="1473682236679" ID="ID_409333865" MODIFIED="1473682558236" TEXT="cmp">
+<node CREATED="1473682689192" ID="ID_980084422" MODIFIED="1473682736846" TEXT="dit(dit1,dit2) &#x6bd4;&#x8f83;&#x4e24;&#x5b57;&#x5178;&#x5143;&#x7d20;"/>
+</node>
+<node CREATED="1473682558711" ID="ID_1016141416" MODIFIED="1473682564101" TEXT="len">
+<node CREATED="1473682739524" ID="ID_564411888" MODIFIED="1473682778077" TEXT="len(dit) &#x8ba1;&#x7b97;&#x5b57;&#x5178;&#x5143;&#x7d20;&#x4e2a;&#x6570;"/>
+</node>
+<node CREATED="1473682564952" ID="ID_915323424" MODIFIED="1473682568893" TEXT="str">
+<node CREATED="1473682781878" ID="ID_1336141774" MODIFIED="1473682817672" TEXT="&#x8f6c;&#x6362;&#xff0c;&#x751f;&#x6210;&#x65b0;&#x7684;&#x5b57;&#x7b26;&#x4e32;&#x5217;&#x8868;"/>
+</node>
+<node CREATED="1473682682457" ID="ID_1736250426" MODIFIED="1473682687130" TEXT="type">
+<node CREATED="1473682824282" ID="ID_1200606896" MODIFIED="1473682844312" TEXT="type(dit) &#x67e5;&#x770b;&#x53d8;&#x91cf;&#x7c7b;&#x578b;"/>
+</node>
+</node>
+<node CREATED="1473682855949" ID="ID_1954582575" MODIFIED="1473682859643" TEXT="&#x65b9;&#x6cd5;">
+<node CREATED="1473683029887" ID="ID_1028144587" MODIFIED="1473683042180" TEXT="clear">
+<node CREATED="1473683209254" ID="ID_900999150" MODIFIED="1473683218348" TEXT="dit.clear()">
+<node CREATED="1473683219449" ID="ID_93206935" MODIFIED="1473683247933" TEXT="&#x6e05;&#x7a7a;&#x5b57;&#x5178;&#x5143;&#x7d20;"/>
+</node>
+</node>
+<node CREATED="1473683069216" ID="ID_1370241620" MODIFIED="1473683070475" TEXT="copy">
+<node CREATED="1473683352558" ID="ID_347448878" MODIFIED="1473683361235" TEXT="dit.copy()">
+<node CREATED="1473683362554" ID="ID_1745236685" MODIFIED="1473683388287" TEXT="&#x6d45;&#x590d;&#x5236;&#x4e00;&#x4e2a;&#x5b57;&#x5178;"/>
+</node>
+</node>
+<node CREATED="1473683076244" ID="ID_1798403809" MODIFIED="1473683077555" TEXT="fromkeys">
+<node CREATED="1473683893345" ID="ID_163941395" MODIFIED="1473684002476" TEXT="dit.fromkeys()">
+<node CREATED="1473683903932" ID="ID_1394144778" MODIFIED="1473683915527">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &gt;&gt;&gt; dit = (1,2,3)
+    </p>
+    <p>
+      &gt;&gt;&gt; dit1 = dit1.fromkeys(dit)
+    </p>
+    <p>
+      &gt;&gt;&gt; dit1
+    </p>
+    <p>
+      {1: None, 2: None, 3: None}
+    </p>
+    <p>
+      &gt;&gt;&gt; dit1 = dit1.fromkeys(dit,3)
+    </p>
+    <p>
+      &gt;&gt;&gt; dit1
+    </p>
+    <p>
+      {1: 3, 2: 3, 3: 3}
+    </p>
+    <p>
+      &gt;&gt;&gt;
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1473683956783" ID="ID_1449154911" MODIFIED="1473683958902" TEXT="&#x521b;&#x5efa;&#x4e00;&#x4e2a;&#x65b0;&#x5b57;&#x5178;&#xff0c;&#x4ee5;&#x5e8f;&#x5217;seq&#x4e2d;&#x5143;&#x7d20;&#x505a;&#x5b57;&#x5178;&#x7684;&#x952e;&#xff0c;val&#x4e3a;&#x5b57;&#x5178;&#x6240;&#x6709;&#x952e;&#x5bf9;&#x5e94;&#x7684;&#x521d;&#x59cb;&#x503c;"/>
+</node>
+</node>
+</node>
+<node CREATED="1473683086440" ID="ID_588636533" MODIFIED="1473683091757" TEXT="get">
+<node CREATED="1473683985902" ID="ID_415341436" MODIFIED="1473684073337" TEXT="dit.get(key)">
+<node CREATED="1473684058344" ID="ID_1975526985" MODIFIED="1473684060129" TEXT="&#x8fd4;&#x56de;&#x6307;&#x5b9a;&#x952e;&#x7684;&#x503c;&#xff0c;&#x5982;&#x679c;&#x503c;&#x4e0d;&#x5728;&#x5b57;&#x5178;&#x4e2d;&#x8fd4;&#x56de;default&#x503c;"/>
+<node CREATED="1473684099835" ID="ID_1665511763" MODIFIED="1473684184168" TEXT="&#x5982;&#x679c;key&#x5b58;&#x5728;&#xff0c;&#x8fd4;&#x56de;value"/>
+<node CREATED="1473684185184" ID="ID_1256719906" MODIFIED="1473684203832" TEXT="&#x5982;&#x679c;key&#x4e0d;&#x5b58;&#x5728;&#xff0c;&#x4e0d;&#x53cd;&#x56de;"/>
+</node>
+<node CREATED="1473684074694" ID="ID_1191341698" MODIFIED="1473684094734" TEXT="dit.get(key,value)">
+<node CREATED="1473684211062" ID="ID_1213796371" MODIFIED="1473684245411" TEXT="&#x5982;&#x679c;key&#x5b58;&#x5728;&#xff0c;&#x8fd4;&#x56de;&#x9ed8;&#x8ba4;value"/>
+</node>
+</node>
+<node CREATED="1473683097161" ID="ID_522372138" MODIFIED="1473683098434" TEXT="has_key">
+<node CREATED="1473684323203" ID="ID_1778953731" MODIFIED="1473684337255" TEXT="dit.has_key(key)">
+<node CREATED="1473684338883" ID="ID_220940614" MODIFIED="1473684341124" TEXT="&#x5982;&#x679c;&#x952e;&#x5728;&#x5b57;&#x5178;dict&#x91cc;&#x8fd4;&#x56de;true&#xff0c;&#x5426;&#x5219;&#x8fd4;&#x56de;false"/>
+</node>
+</node>
+<node CREATED="1473683099773" ID="ID_1530700166" MODIFIED="1473683105751" TEXT="items">
+<node CREATED="1473684563237" ID="ID_1458424597" MODIFIED="1473684570836" TEXT="dit.items()">
+<node CREATED="1473684571689" ID="ID_38429165" MODIFIED="1473684587665" TEXT="&#x8fd4;&#x56de;&#x53ef;&#x904d;&#x5386;&#x7684;&#x5e8f;&#x5217;"/>
+</node>
+</node>
+<node CREATED="1473683135451" ID="ID_1938858157" MODIFIED="1473683136228" TEXT="keys">
+<node CREATED="1473684701159" ID="ID_1924315732" MODIFIED="1473684707825" TEXT="dit.keys()">
+<node CREATED="1473684868601" ID="ID_1629238976" MODIFIED="1473684876288" TEXT="&#x4ee5;&#x5217;&#x8868;&#x8fd4;&#x56de;&#x4e00;&#x4e2a;&#x5b57;&#x5178;&#x6240;&#x6709;&#x7684;key"/>
+</node>
+</node>
+<node CREATED="1473686038077" ID="ID_581022315" MODIFIED="1473686043409" TEXT="values">
+<node CREATED="1473686044296" ID="ID_1692635612" MODIFIED="1473686053989" TEXT="dit.values()">
+<node CREATED="1473686054859" ID="ID_1902390542" MODIFIED="1473686082109" TEXT="&#x4ee5;&#x5217;&#x8868;&#x8fd4;&#x56de;&#x5b57;&#x5178;&#x6240;&#x6709;value"/>
+</node>
+</node>
+<node CREATED="1473685924354" ID="ID_1130794096" MODIFIED="1473685927192" TEXT="copy">
+<node CREATED="1473685929399" ID="ID_284355262" MODIFIED="1473685967495" TEXT="dit.copy()">
+<node CREATED="1473685969127" ID="ID_1600231613" MODIFIED="1473685985088" TEXT="&#x590d;&#x5236;&#x5b57;&#x5178;"/>
+</node>
+</node>
+<node CREATED="1473683140830" ID="ID_1320514265" MODIFIED="1473683141568" TEXT="pop">
+<node CREATED="1473686172995" ID="ID_796361596" MODIFIED="1473686187779" TEXT="dit.pop(1,2)">
+<node CREATED="1473686188660" ID="ID_1018079303" MODIFIED="1473686197377">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &gt;&gt;&gt; users.pop('pc',30)
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;30
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;&gt;&gt;&gt; users
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;{'ww': 23}
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;&gt;&gt;&gt; users.pop('ww',30)
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;23
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;&gt;&gt;&gt; users.pop('ww',30)
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;30
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1473686203296" ID="ID_762773696" MODIFIED="1473686220513" TEXT="&#x5f39;&#x51fa;&#x5143;&#x7d20;"/>
+</node>
+</node>
+</node>
+<node CREATED="1473683146971" ID="ID_1838550297" MODIFIED="1473683147942" TEXT="popitem">
+<node CREATED="1473686503074" ID="ID_304270514" MODIFIED="1473686546344" TEXT="dit.popitem()">
+<node CREATED="1473686547361" ID="ID_1219523777" MODIFIED="1473686562478" TEXT="&gt;&gt;&gt; users = dict.fromkeys([&apos;kk&apos;,&apos;wp&apos;],29)&#xa;    &gt;&gt;&gt; users&#xa;    {&apos;kk&apos;: 29, &apos;wp&apos;: 29}&#xa;    &gt;&gt;&gt; users.popitem()&#xa;    (&apos;kk&apos;, 29)&#xa;    &gt;&gt;&gt; users.popitem()&#xa;    (&apos;wp&apos;, 29)&#xa;    &gt;&gt;&gt; users.popitem()&#xa;    Traceback (most recent call last):&#xa;      File &quot;&lt;stdin&gt;&quot;, line 1, in &lt;module&gt;&#xa;    KeyError: &apos;popitem(): dictionary is empty&apos;">
+<node CREATED="1473686564665" ID="ID_583026195" MODIFIED="1473686577306" TEXT="&#x5f39;&#x51fa;&#x5143;&#x7d20;"/>
+</node>
+</node>
+</node>
+<node CREATED="1473683152854" ID="ID_556372965" MODIFIED="1473683153894" TEXT="setdefault">
+<node CREATED="1473685380133" ID="ID_474467689" MODIFIED="1473685389436" TEXT="dit.setdefault()">
+<node CREATED="1473685390565" ID="ID_170783831" MODIFIED="1473685392159" TEXT="&#x548c;get()&#x7c7b;&#x4f3c;, &#x4f46;&#x5982;&#x679c;&#x952e;&#x4e0d;&#x5b58;&#x5728;&#x4e8e;&#x5b57;&#x5178;&#x4e2d;&#xff0c;&#x5c06;&#x4f1a;&#x6dfb;&#x52a0;&#x952e;&#x5e76;&#x5c06;&#x503c;&#x8bbe;&#x4e3a;default"/>
+</node>
+</node>
+<node CREATED="1473683158365" ID="ID_187220382" MODIFIED="1473683159270" TEXT="update">
+<node CREATED="1473685689589" ID="ID_648034448" MODIFIED="1473685702089" TEXT="dit.update(dit2)">
+<node CREATED="1473685722461" ID="ID_765038014" MODIFIED="1473685761410" TEXT="&#x628a;&#x5b57;&#x5178;dit2&#x7684;&#x952e;/&#x503c;&#x5bf9;&#x66f4;&#x65b0;&#x5230;dit&#x91cc;"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1473680674026" ID="ID_1243266865" MODIFIED="1473686678799" POSITION="right" TEXT="&#x6587;&#x4ef6;">
+<node CREATED="1473686699111" ID="ID_1219574250" MODIFIED="1473686704183" TEXT="&#x6587;&#x4ef6;&#x4f4d;&#x7f6e;">
+<node CREATED="1473686773389" ID="ID_933231751" MODIFIED="1473686777928" TEXT="&#x6587;&#x4ef6;&#x8def;&#x5f84;">
+<node CREATED="1473686809721" ID="ID_817986111" MODIFIED="1473686818409" TEXT="&#x76f8;&#x5bf9;&#x8def;&#x5f84;"/>
+<node CREATED="1473686818966" ID="ID_1446287448" MODIFIED="1473686822855" TEXT="&#x7edd;&#x5bf9;&#x8def;&#x5f84;"/>
+</node>
+</node>
+<node CREATED="1473686704742" ID="ID_297874007" MODIFIED="1473686707665" TEXT="&#x6253;&#x5f00;">
+<node CREATED="1473686840828" ID="ID_856145144" MODIFIED="1473686842444" TEXT="fhandler = open(name,mode=&apos;r&apos;)">
+<node CREATED="1473686848383" ID="ID_972794155" MODIFIED="1473686850261" TEXT="r">
+<node CREATED="1473686874830" ID="ID_1299519245" MODIFIED="1473686908163" TEXT="&#x6587;&#x4ef6;&#x5fc5;&#x987b;&#x5b58;&#x5728;&#xff0c;&#x4e0d;&#x5b58;&#x5728;&#x62a5;&#x9519;"/>
+<node CREATED="1473686939374" ID="ID_1895572428" MODIFIED="1473686954319">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#160;read&#160;&#160;&#20840;&#37096;&#35835;&#21462;
+    </p>
+    <p>
+      &#160;read&#65288;size&#65289; &#35835;&#21462;szie&#22823;&#23567;
+    </p>
+    <p>
+      &#160;readlien &#35835;&#21462;&#19968;&#34892;
+    </p>
+    <p>
+      &#160;readliens
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1473686851124" ID="ID_793555965" MODIFIED="1473686852560" TEXT="w">
+<node CREATED="1473686963684" ID="ID_793129310" MODIFIED="1473686965033" TEXT="&#x5982;&#x679c;&#x6587;&#x4ef6;&#x4e0d;&#x5b58;&#x5728;&#xff0c;&#x5219;&#x521b;&#x5efa;"/>
+<node CREATED="1473686965340" ID="ID_417528472" MODIFIED="1473686978731" TEXT="&#x6587;&#x4ef6;&#x5b58;&#x5728;&#xff0c;&#x8986;&#x76d6;"/>
+<node CREATED="1473686988379" ID="ID_1876840939" MODIFIED="1473687470947">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      write&#160; <font color="rgb(51, 51, 51)" face="Microsoft Yahei, Helvetica Neue, Helvetica, Arial, sans-serif" size="12px">&#23558;&#23383;&#31526;&#20018;&#20889;&#20837;&#25991;&#20214;&#65292;&#27809;&#26377;&#36820;&#22238;&#20540;</font>
+    </p>
+    <p>
+      writelines <font color="rgb(51, 51, 51)" face="Microsoft Yahei, Helvetica Neue, Helvetica, Arial, sans-serif" size="12px">&#21521;&#25991;&#20214;&#20889;&#20837;&#19968;&#20010;&#24207;&#21015;&#23383;&#31526;&#20018;&#21015;&#34920;&#65292;&#22914;&#26524;&#38656;&#35201;&#25442;&#34892;&#21017;&#35201;&#33258;&#24049;&#21152;&#20837;&#27599;&#34892;&#30340;&#25442;&#34892;&#31526;&#12290;</font>
+    </p>
+    <p>
+      flush&#160; <font color="rgb(51, 51, 51)" face="Microsoft Yahei, Helvetica Neue, Helvetica, Arial, sans-serif" size="12px">&#21047;&#26032;&#25991;&#20214;&#20869;&#37096;&#32531;&#20914;</font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1473686853140" ID="ID_1745379490" MODIFIED="1473686855640" TEXT="a">
+<node CREATED="1473687034526" ID="ID_502202251" MODIFIED="1473687039963" TEXT="&#x8ffd;&#x52a0;"/>
+</node>
+<node CREATED="1473687052255" ID="ID_1897794549" MODIFIED="1473687115084" TEXT="wr+">
+<node CREATED="1473687092937" ID="ID_413320772" MODIFIED="1473687094376" TEXT="&#x6587;&#x4ef6;&#x5b58;&#x5728;&#x5219;&#x8986;&#x76d6;         &#x6587;&#x4ef6;&#x4e0d;&#x5b58;&#x5728;&#xff0c;&#x5219;&#x521b;&#x5efa;"/>
+</node>
+<node CREATED="1473687059969" ID="ID_135601733" MODIFIED="1473687119887" TEXT="rw+">
+<node CREATED="1473687121302" ID="ID_1846461598" MODIFIED="1473687123220" TEXT="&#x6587;&#x4ef6;&#x5b58;&#x5728;&#x4e0d;&#x8986;&#x76d6;         &#x6587;&#x4ef6;&#x4e0d;&#x5b58;&#x5728;&#x5219;&#x62a5;&#x9519;"/>
+</node>
+<node CREATED="1473687064512" ID="ID_1817464368" MODIFIED="1473687073190" TEXT="ra+">
+<node CREATED="1473687131575" ID="ID_71020119" MODIFIED="1473687132697" TEXT="rw+ &#x4e00;&#x6837;"/>
+</node>
+<node CREATED="1473687073654" ID="ID_391430489" MODIFIED="1473687079466" TEXT="ar+">
+<node CREATED="1473687138367" ID="ID_1371261385" MODIFIED="1473687139341" TEXT="&#x8ffd;&#x52a0;&#x5230;&#x6700;&#x540e;"/>
+</node>
+</node>
+<node CREATED="1473687248602" ID="ID_864623078" MODIFIED="1473687251005" TEXT="tell">
+<node CREATED="1473687252465" ID="ID_626122223" MODIFIED="1473687260924" TEXT="file.tell()&#x8fd4;&#x56de;&#x6587;&#x4ef6;&#x5f53;&#x524d;&#x4f4d;&#x7f6e;"/>
+</node>
+<node CREATED="1473687278638" ID="ID_1517661122" MODIFIED="1473687280736" TEXT="seek">
+<node CREATED="1473687281782" ID="ID_238351946" MODIFIED="1473687326290" TEXT="file.seek(offset,[whence])&#x8bbe;&#x7f6e;&#x6587;&#x4ef6;&#x5f53;&#x524d;&#x4f4d;&#x7f6e;"/>
+</node>
+<node CREATED="1473687352764" ID="ID_1281367581" MODIFIED="1473687357833" TEXT="flush">
+<node CREATED="1473687361074" ID="ID_1814928062" MODIFIED="1473687369836" TEXT="file.flush()&#x5237;&#x65b0;&#x6587;&#x4ef6;&#x5185;&#x90e8;&#x7f13;&#x51b2;&#xff0c;&#x76f4;&#x63a5;&#x628a;&#x5185;&#x90e8;&#x7f13;&#x51b2;&#x533a;&#x7684;&#x6570;&#x636e;&#x7acb;&#x523b;&#x5199;&#x5165;&#x6587;&#x4ef6;, &#x800c;&#x4e0d;&#x662f;&#x88ab;&#x52a8;&#x7684;&#x7b49;&#x5f85;&#x8f93;&#x51fa;&#x7f13;&#x51b2;&#x533a;&#x5199;&#x5165;&#x3002;"/>
+</node>
+</node>
+<node CREATED="1473686708050" ID="ID_464576205" MODIFIED="1473686723499" TEXT="&#x5173;&#x95ed;">
+<node CREATED="1473687156857" ID="ID_1436587707" MODIFIED="1473687342489" TEXT="file.close()"/>
+</node>
+</node>
+</node>
+</map>
