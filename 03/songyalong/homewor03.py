0c026c734770064f7902cab465f176befa954fbe
宋亚龙 <851096287@qq.com>
2016-09-18 18:08:06 +0800

    作业前两道题，最后一道有时间后续补上

---
+++ 03/songyalong/homewor03.py
@@ -0,0 +1,24 @@
+#encoding:utf-8
+'''copy文件'''
+# file_01=open('03.txt','rb')
+# file_02=open('03new.txt','wb')
+# while True:
+#     content_01=file_01.readline()
+#     if content_01 != '':
+#         file_02.write(file_01.readline())
+#     else:
+#         break
+# file_01.close()
+# file_02.close()
+
+'''tail实现'''
+# file_tail=open('03.txt','r')
+#
+# file_tail.seek(0, 2)
+# while True:
+#     content_str = ''
+#     content_str+=file_tail.read()
+#     if content_str.endswith('\n'):
+#         print content_str,
+
+
