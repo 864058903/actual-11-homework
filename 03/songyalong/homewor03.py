7ff0aa2cacade2b4bf050a89b2cc570ea19d267b
宋亚龙 <851096287@qq.com>
2016-09-19 16:44:49 +0800

    第三道题提交

--- 03/songyalong/homewor03.py
+++ 03/songyalong/homewor03.py
@@ -20,5 +20,23 @@
 #     content_str+=file_tail.read()
 #     if content_str.endswith('\n'):
 #         print content_str,
+file_log=open('song.log','r')
+data_list=[]
+while True:
+    content_line_str=''
+    if  file_log.readline():
+        content_line=file_log.readline()
+        tup_tmp=(content_line.split(' ')[0],content_line.split(' ')[6],content_line.split(' ')[8])
+        data_list.append(tup_tmp)
+    else:
+        break
+file_log.close()
+cnt_list={}
+for i in data_list:
+    if i in cnt_list:
+        cnt_list[i]=cnt_list[i]+1
+    else:
+        cnt_list[i]=1
+print '统计数据为：\n',cnt_list
 
 
