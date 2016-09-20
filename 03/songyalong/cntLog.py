944abbae300a582f8bab23b0bba9c699987b7bc5
宋亚龙 <851096287@qq.com>
2016-09-20 11:43:53 +0800

    代码分开写，第三道题的答案写到文件里去

--- 03/songyalong/cntLog.py
+++ 03/songyalong/cntLog.py
@@ -1,25 +1,4 @@
 #encoding:utf-8
-'''copy文件'''
-# file_01=open('03.txt','rb')
-# file_02=open('03new.txt','wb')
-# while True:
-#     content_01=file_01.readline()
-#     if content_01 != '':
-#         file_02.write(file_01.readline())
-#     else:
-#         break
-# file_01.close()
-# file_02.close()
-
-'''tail实现'''
-# file_tail=open('03.txt','r')
-#
-# file_tail.seek(0, 2)
-# while True:
-#     content_str = ''
-#     content_str+=file_tail.read()
-#     if content_str.endswith('\n'):
-#         print content_str,
 '''统计日志'''
 file_log=open('song.log','r')
 data_list=[]
@@ -39,5 +18,10 @@ for i in data_list:
     else:
         cnt_list[i]=1
 print '统计数据为：\n',cnt_list
-
+cnt_file=open('contFile.txt','w')
+'''把统计的内容写到文件里'''
+for i in cnt_list:
+    # cnt_file.write(str(i))
+    cnt_file.write(str(i)+'出现的次数为：'+str(cnt_list[i])+'\n')
+cnt_file.close()
 
---
+++ 03/songyalong/tailFile.py
@@ -0,0 +1,11 @@
+#encoding:utf-8
+
+'''tail实现'''
+# file_tail=open('03.txt','r')
+#
+# file_tail.seek(0, 2)
+# while True:
+#     content_str = ''
+#     content_str+=file_tail.read()
+#     if content_str.endswith('\n'):
+#         print content_str,
