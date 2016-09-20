a931dbd2091703f25d651aea7d242ae365afb9d3
seoguess <seoguess@qq.com>
2016-09-20 21:36:18 +0800

    add ip,url,code --> count

--- 03/shawn/log_analysis.py
+++ 03/shawn/log_analysis.py
@@ -7,15 +7,7 @@ path = "www_access_20140823.log"
 f1 = open(path, 'r')
 
 """
-索引：
-
-ip_dict --> ip:url
-url_dict --> url:code
-code_dict --> code:url
-
 --> {(ip,url,code): count}
-
-
 """
 
 # value
@@ -31,30 +23,33 @@ for line in scnt:
     all_dict.setdefault((ip,url,code),0)
     all_dict[(ip,url,code)] += 0
 
-# ip_dict
+# print all_dict
+
+# def function  
 
 def len_detail(num):
-    ip_dict = {}
-
+    inner_dict = {}
     for line in scnt:
-        ip_dict.setdefault(line[num],0)
-        ip_dict[line[num]] += 1
-
-    print len(ip_dict)
-
-    sort_ip = sorted(ip_dict.items(), key=lambda x: x[1], reverse = True )
-
-    for x,y in sort_ip:
-        print y,x
-
-len_detail(0)
-len_detail(6)
-len_detail(8)
+        inner_dict.setdefault(line[num],0)
+        inner_dict[line[num]] += 1
+    return inner_dict 
 
 
+ip_dict = len_detail(0)
+url_dict = len_detail(6)
+code_dict = len_detail(8)
 
-#print ip_dict.keys()
+print "Total ip: %s" % len(ip_dict)
+print "Total url: %s" % len(url_dict)
+print "Total code: %s" % len(code_dict)
+print ""
 
+# print code,count 
+code_list = sorted( code_dict.items(), key=lambda x: x[1], reverse = True )
+print "Print code vs count...."
+print ""
+for x,y in code_list:
+	print "{0} \t {1}".format(y,x)
 
 # close file
 f1.close()
