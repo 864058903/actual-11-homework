33c03390d6ff84bf5010698207486a0133401da2
weibzd <157856789@qq.com>
2016-09-26 17:20:24 +0800

    'wk'

--- 04/wanghaibing/homework05.py
+++ 04/wanghaibing/homework05.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 #encoding:utf-8
-unsort_list2=[(1,2),(6,3),(4,1),(2,10),(3,5),(5,7)]
+#arg1:要排序的序列，arg2:max、min最大最小排序; arg3:0、False升序，1、True降序
+def sorts(x,y=max,z=0):
+    return sorted(x,key=lambda a:y(a),reverse=z)
 
-def sorts(x):
-    return sorted(x,key=lambda a:max(a))
-
-print sorts(unsort_list2)
+if __name__=='__main__':
+   unsort_list=[(1,2),(6,3),(4,1),(2,10),(3,5),(5,7)]
+   print sorts(unsort_list,max,1)
