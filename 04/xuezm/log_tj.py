46552a2ea16a24e02f014c4bf8f2ff30b2c85e5c
root <root@i-asr0tbc8.(none)>
2016-09-27 09:57:10 +0800

    update

--- 04/xuezm/log_tj.py
+++ 04/xuezm/log_tj.py
@@ -7,15 +7,13 @@ def access_log_tj(logpath,dsplogpath,topn):
         for  i in  f.readlines():
             s=i.split(" ")
             ip,url,code=s[0],s[6],s[8]
-            #print ip,url,code
             zd[ip,url,code]=zd.get((ip,url,code),0)+1
-    #print zd
     result= sorted(zd.items(),key=lambda x:x[1],reverse=True)
     result=result[:topn]
     with open('dsp.log','w+') as f:
-        for  i in   result:
-            f.write(str(i)+'\n')
+        for  k,v in  dict(result).items():
+            f.write(" ".join(k) + " " + str(v) + '\n')
     return "success"
 
 if  __name__   == "__main__":
-     print access_log_tj('access.log','dsp.log',10)
+    print access_log_tj('access.log','dsp.log',10)
