87c9b26d47066813f360ba65ee22a711e1f1caf1
weibzd <157856789@qq.com>
2016-09-27 12:36:44 +0800

    'wk'

--- 04/wanghaibing/homework04.py
+++ 04/wanghaibing/homework04.py
@@ -9,12 +9,10 @@ def sorts(log_file,dst_file,y=1,n=10):
    num=1
    for line in r_file:
        key=line.split()
-       key=(key[0],key[8],key[10])
+       key=key[0],key[8],key[10]
        r_dict[key]=r_dict.setdefault(key,0)+1
-   for k,v in r_dict.items():
-       tmplist.append((k,v))
 
-   tmplist=sorted(tmplist,key=lambda x:x[1],reverse=y)
+   tmplist=sorted(r_dict.items(),key=lambda r_dict:r_dict[1],reverse=y)
    for i in tmplist:
         if num <= n:
            w_file.write('%s %s\n' %(i[1],i[0]))
