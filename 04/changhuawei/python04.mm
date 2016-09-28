09f218ebe54f787ccb7250be7ebdee84881e6edb
huaweichang <513314416@qq.com>
2016-09-28 18:12:42 +0800

    update

---
+++ 04/changhuawei/python04.mm
@@ -0,0 +1,217 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1474966824435" ID="ID_738738834" MODIFIED="1474966843421" TEXT="python04">
+<node CREATED="1474966849558" ID="ID_1825181640" MODIFIED="1474966873783" POSITION="right" TEXT="&#x51fd;&#x6570;">
+<node CREATED="1474967328907" ID="ID_41619433" MODIFIED="1474967331188" TEXT="&#x5b9a;&#x4e49;">
+<node CREATED="1474967615957" ID="ID_751981570" MODIFIED="1474967651728" TEXT="&#x51fd;&#x6570;&#x662f;&#x7ec4;&#x7ec7;&#x597d;&#x7684;&#xff0c;&#x53ef;&#x91cd;&#x590d;&#x4f7f;&#x7528;&#x7684;&#xff0c;&#x7528;&#x6765;&#x5b9e;&#x73b0;&#x5355;&#x4e00;&#xff0c;&#x6216;&#x76f8;&#x5173;&#x8054;&#x529f;&#x80fd;&#x7684;&#x4ee3;&#x7801;&#x6bb5;"/>
+<node CREATED="1474978130076" ID="ID_291688729" MODIFIED="1474978165819" TEXT="def func_name():&#xa;     func_code"/>
+</node>
+<node CREATED="1474967340481" ID="ID_787758007" MODIFIED="1474967345461" TEXT="&#x8c03;&#x7528;">
+<node CREATED="1474978244968" ID="ID_1183115275" MODIFIED="1474978282962">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: left">
+      func_name()
+    </p>
+    <p style="text-align: left">
+      &#21015;&#34920;&#21442;&#25968;&#22312;&#21518;&#65292;&#20851;&#38190;&#23383;&#21442;&#25968;&#22312;&#21069;&#23545;&#24212;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1474967574881" ID="ID_1835328886" MODIFIED="1474967594457" TEXT="&#x683c;&#x5f0f;">
+<node CREATED="1474978313525" ID="ID_1972750232" MODIFIED="1474978318992">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#160;&#160;&#160;1&#65292;&#20989;&#25968;&#21517;+&#20989;&#25968;&#20307;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;2&#65292;&#36755;&#20837;&#65288;&#21442;&#25968;&#65289;/&#36755;&#20986;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;3&#65292;&#21442;&#25968;&#40664;&#35748;&#20540;&#65288;&#40664;&#35748;&#20540;&#21442;&#25968;&#24517;&#39035;&#22312;&#21518;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1474967595172" ID="ID_776789658" MODIFIED="1474967602179" TEXT="LGB&#x539f;&#x5219;">
+<node CREATED="1474978505971" ID="ID_1834918691" MODIFIED="1474983867644">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      1,locals&#160;&#160;&#160;&#26412;&#22320;
+    </p>
+    <p>
+      2,globals &#20840;&#23616;
+    </p>
+    <p>
+      3,builtin&#160;&#160;&#20869;&#32622;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1474967608122" ID="ID_1201146966" MODIFIED="1474967612322" TEXT="&#x53ef;&#x53d8;&#x53c2;&#x6570;">
+<node CREATED="1474980118518" ID="ID_352698057" MODIFIED="1474982212284">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#21015;&#34920;/&#23383;&#20856;
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;def fun1(*args,**kargs):
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;print args,kargs
+    </p>
+    <p>
+      &gt;&gt;&gt; def fun1(*args,**kwargs):
+    </p>
+    <p>
+      ...&#160;&#160;&#160;&#160;&#160;print args,kwargs
+    </p>
+    <p>
+      &gt;&gt;&gt; fun1(range(3),age=20)
+    </p>
+    <p>
+      ([0, 1, 2],) {'age': 20}
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;def fun2(*args):
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;print args
+    </p>
+    <p>
+      &gt;&gt;&gt; def fun2(*args):
+    </p>
+    <p>
+      ...&#160;&#160;&#160;&#160;&#160;print args
+    </p>
+    <p>
+      &gt;&gt;&gt; fun2(1)
+    </p>
+    <p>
+      (1,)
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;def fun3(**kargs):
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;print kargs
+    </p>
+    <p>
+      &gt;&gt;&gt; def aa(**kwargs):
+    </p>
+    <p>
+      ...&#160;&#160;&#160;&#160;&#160;print kwargs
+    </p>
+    <p>
+      ...
+    </p>
+    <p>
+      &gt;&gt;&gt; aa(a=1)
+    </p>
+    <p>
+      {'a': 1}
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1474982240352" ID="ID_1087146407" MODIFIED="1474982242845" TEXT="&#x5217;&#x8868;&#x63a8;&#x5bfc;&#x5f0f;">
+<node CREATED="1474982249575" ID="ID_791410361" MODIFIED="1474982253100" TEXT="&#x5b9a;&#x4e49;">
+<node CREATED="1474982254608" ID="ID_1849011969" MODIFIED="1474982255885" TEXT="&#x7531;&#x4e00;&#x4e2a;list&#x4e2d;&#x7684;&#xff08;&#x6ee1;&#x8db3;&#x6761;&#x4ef6;&#x7684;&#xff09;&#x5143;&#x7d20;&#x901a;&#x8fc7;&#x67d0;&#x79cd;&#x8f6c;&#x6362;&#x5f97;&#x5230;&#x7684;&#x5143;&#x7d20;&#x7ec4;&#x6210;&#x7684;list"/>
+</node>
+<node CREATED="1474982735334" ID="ID_1134602564" MODIFIED="1474982750523" TEXT="&#x793a;&#x4f8b;">
+<node CREATED="1474982751617" ID="ID_948001432" MODIFIED="1474982758916">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &gt;&gt;&gt; aa = [1,2,3,4]
+    </p>
+    <p>
+      &gt;&gt;&gt; print [x**2 for x in aa]
+    </p>
+    <p>
+      [1, 4, 9, 16]
+    </p>
+    <p>
+      &gt;&gt;&gt; print [x**2 for x in aa if x %2==0]
+    </p>
+    <p>
+      [4, 16]
+    </p>
+    <p>
+      &gt;&gt;&gt; print dict([(x,x*10) for x in aa])
+    </p>
+    <p>
+      {1: 10, 2: 20, 3: 30, 4: 40}
+    </p>
+    <p>
+      &gt;&gt;&gt; print [x*y for x in [1,2,3] for y in&#160;&#160;[1,2,3]]
+    </p>
+    <p>
+      [1, 2, 3, 2, 4, 6, 3, 6, 9]
+    </p>
+    <p>
+      &gt;&gt;&gt; testList = [1,2,3,4]
+    </p>
+    <p>
+      &gt;&gt;&gt; def mul2(x):
+    </p>
+    <p>
+      ...&#160;&#160;&#160;&#160;&#160;return x*2
+    </p>
+    <p>
+      ...
+    </p>
+    <p>
+      &gt;&gt;&gt; print [mul2(i) for i in testList]
+    </p>
+    <p>
+      [2, 4, 6, 8]
+    </p>
+    <p>
+      &gt;&gt;&gt;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1474966887609" ID="ID_1155892398" MODIFIED="1474966894485" POSITION="right" TEXT="&#x6a21;&#x5757;">
+<node CREATED="1474982806944" ID="ID_1505515113" MODIFIED="1474982916942" TEXT="&#x5b9a;&#x4e49;">
+<node CREATED="1474982917731" ID="ID_502635383" MODIFIED="1474982918852" TEXT="&#x6a21;&#x5757;&#x5c31;&#x662f;&#x4e00;&#x4e2a;&#x4fdd;&#x5b58;&#x4e86;Python&#x4ee3;&#x7801;&#x7684;&#x6587;&#x4ef6;&#x3002;&#x6a21;&#x5757;&#x80fd;&#x5b9a;&#x4e49;&#x51fd;&#x6570;&#xff0c;&#x7c7b;&#x548c;&#x53d8;&#x91cf;&#x3002;&#x6a21;&#x5757;&#x91cc;&#x4e5f;&#x80fd;&#x5305;&#x542b;&#x53ef;&#x6267;&#x884c;&#x7684;&#x4ee3;&#x7801;&#x3002;"/>
+</node>
+<node CREATED="1474982897103" ID="ID_1575912697" MODIFIED="1474983823017" TEXT="import"/>
+</node>
+</node>
+</map>
